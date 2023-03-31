bbba85701b0dd65276b61679b091bcc904a6bf85
lhyeonj120 <isk04387@gmail.com>
2023-03-31 14:52:49 +0900

    add coffee-status mapper, vo, service, controller

--- coffee-status/src/main/java/com/example/coffeestatus/controller/CoffeeStatusRestController.java
+++ coffee-status/src/main/java/com/example/coffeestatus/controller/CoffeeStatusRestController.java
@@ -1,15 +1,37 @@
 package com.example.coffeestatus.controller;
 
+import com.example.coffeestatus.service.CoffeeStatusService;
+import com.example.coffeestatus.vo.CoffeeStatusVO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/coffee-status")
 public class CoffeeStatusRestController {
 
+    @Autowired
+    private CoffeeStatusService coffeeStatusService;
+
     @GetMapping("/")
     public String coffeeStatus(){
         return "coffee-status service";
+    }
+
+    @GetMapping("/createStatusTable")
+    public void createStatusTable(){
+        coffeeStatusService.createStatusTable();
+    }
+
+    @GetMapping("/coffeeOrderStatus")
+    public ResponseEntity<List<CoffeeStatusVO>> coffeeOrderStatus(){
+        List<CoffeeStatusVO> list = coffeeStatusService.selectCoffeeOrderStatus();
+
+        return new ResponseEntity<List<CoffeeStatusVO>>(list, HttpStatus.OK);
     }
 }
