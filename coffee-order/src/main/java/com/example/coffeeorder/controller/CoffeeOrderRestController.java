e931dff73ea524fc654d880acb3c82912aa0a37a
lhyeonj120 <isk04387@gmail.com>
2023-03-31 14:03:07 +0900

    add CoffeeOrderRestController

--- coffee-order/src/main/java/com/example/coffeeorder/controller/CoffeeOrderRestController.java
+++ coffee-order/src/main/java/com/example/coffeeorder/controller/CoffeeOrderRestController.java
@@ -1,15 +1,31 @@
 package com.example.coffeeorder.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.coffeeorder.service.CoffeeOrderService;
+import com.example.coffeeorder.vo.CoffeeOrderVO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/coffee-order")
 public class CoffeeOrderRestController {
 
+    @Autowired
+    CoffeeOrderService coffeeOrderService;
+
     @GetMapping("/")
     public String coffeeOrder(){
         return "coffee-order service";
+    }
+
+    @PostMapping("/coffeeOrder")
+    public ResponseEntity<CoffeeOrderVO> coffeeOrder(@RequestBody CoffeeOrderVO vo){
+        // 회원 유무 체크
+
+        //커피 주문
+        coffeeOrderService.coffeeOrder(vo);
+
+        return new ResponseEntity<CoffeeOrderVO>(vo, HttpStatus.OK);
     }
 }
